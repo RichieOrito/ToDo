b5a6cf16678f31fdb77bd04d6c67185c7099274e
RichieOrito <oritorichie2@gmail.com>
2022-05-13 04:57:37 +0300

    Creates the trash mark button

--- js/app.js
+++ js/app.js
@@ -21,4 +21,11 @@ function addTodo(event){
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton);
+    //Check trash btn
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to List
+    todoList.appendChild(todoDiv);
 }
