aa05dbbee94f7fa7d437337984ba5b9b37d9795e
RichieOrito <oritorichie2@gmail.com>
2022-05-19 01:19:22 +0300

    creats a deleteCheck function with the delete functionality

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 //Functions
 function addTodo(event){
     //prevents form from submitting
@@ -30,5 +31,13 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     //Clear Todo Input Value
     todoInput.value="";
+}
 
+function deleteCheck(e){
+    const item = e.target;
+    //Delete Todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
 }
