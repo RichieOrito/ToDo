1bed67f83de66b0fe3317f5e11f25c5d673c4b6e
RichieOrito <oritorichie2@gmail.com>
2022-05-24 23:00:46 +0300

    add todo to local storage

--- js/app.js
+++ js/app.js
@@ -20,6 +20,8 @@ function addTodo(event){
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+    //Add todo to localstorage
+    saveLocalTodos(todoInput.value);
     //Check mark btn
     const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
