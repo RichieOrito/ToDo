b45b7451dd64d4713e5d917cdb084210bac1dd7d
Richard Orito <oritorichie2@gmail.com>
2023-10-18 16:10:58 +0300

    generated the whole code in typescript/nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,128 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
 
-export default function Home() {
+import React, { useEffect, useState } from 'react';
+import { Indie_Flower, Shadows_Into_Light } from '@next/font/google';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusSquare, faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const indie = Indie_Flower({
+  subsets: ['latin'],
+  weight: ['400']
+});
+
+const shadows = Shadows_Into_Light({
+  subsets: ['latin'],
+  weight: ['400']
+});
+
+function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todoInput, setTodoInput] = useState<string>('');
+  const [filter, setFilter] = useState<string>('all');
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const addTodo = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (todoInput.trim() !== '') {
+      const newTodos = [...todos, { text: todoInput, completed: false }];
+      setTodos(newTodos);
+      saveLocalTodos(newTodos);
+      setTodoInput('');
+    }
+  };
+
+  const deleteTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    removeLocalTodos(newTodos);
+  };
+
+  const completeTodo = (index: number) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+    saveLocalTodos(newTodos);
+  };
+
+  const getTodos = () => {
+    const storedTodos = localStorage.getItem('todos');
+    if (storedTodos) {
+      setTodos(JSON.parse(storedTodos));
+    }
+  };
+
+  const saveLocalTodos = (todosToSave: string[]) => {
+    localStorage.setItem('todos', JSON.stringify(todosToSave));
+  };
+
+  const removeLocalTodos = (todosToRemove: string[]) => {
+    localStorage.setItem('todos', JSON.stringify(todosToRemove));
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'all') return true;
+    if (filter === 'completed' && todo.completed) return true;
+    if (filter === 'uncompleted' && !todo.completed) return true;
+    return false;
+  });
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="App">
+      <header className={indie.className}>
+        <h1>Ori&#39;s Todo list</h1>
+      </header>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          className="todo-input"
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
         />
+        <button className="todo-button" type="submit">
+          <FontAwesomeIcon icon={faPlusSquare} />
+        </button>
+        <div className="select">
+          <select
+            name="todos"
+            className="filter-todo"
+            value={filter}
+            onChange={(e) => setFilter(e.target.value)}
+          >
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+      <div className="todo-container">
+        <ul className="todo-list">
+          {filteredTodos.map((todo, index) => (
+            <div key={index} className={`todo ${todo.deleted ? 'fall' : ''}`}>
+              <li
+                className={
+                  todo.completed
+                    ? 'todo-item completed'
+                    : `todo-item ${indie.className}`
+                }
+              >
+                {todo.text}
+              </li>
+              <button className="complete-btn" onClick={() => completeTodo(index)}>
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+              <button className="trash-btn" onClick={() => deleteTodo(index)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </div>
+          ))}
+        </ul>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+export default App;
